f9d892d3c040d009b698ab219a764a8c8f400265
Nitish2512 <tiwarieee2@gmail.com>
2025-10-15 17:35:27 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,12 @@
+# Enter 1st number
+num1 = int(input("Enter 1st number"))
+# Enter 2nd number
+num2 = int(input("Enter 2nd number"))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", round(division,1))
---
+++ Assignment1_Task2.py
@@ -0,0 +1,6 @@
+#Enter 1st name
+first_name = input("Enter your first name:")
+#Enter last name
+last_name = input("Enter your last name:")
+
+print ("Hello,", first_name + " " + last_name + "!", "Welcome to the Python program.")
